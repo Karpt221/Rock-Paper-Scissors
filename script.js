6d4ade34866255ecbffcd6b43365c7a8219272c5
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2024-07-09 15:37:27 +0300

    Add GUI to the Rock-Paper-Scissors

--- script.js
+++ script.js
@@ -1,89 +1,109 @@
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    console.log("Let the game begin!");
-
-    for(let i = 0; i < 5; i++){
-        console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-
-        let winner = playRound(humanChoice, computerChoice);
-        switch(winner){
-            case "nobody": 
-            break;
-            case "human": humanScore++;
-            break;
-            case "computer": computerScore++;
-            break;
-        }
-    }
-    console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
-    if(humanScore > computerScore){
-        console.log('You won this game!');
-    }else if(humanScore < computerScore){
-        console.log('You lost this game!');
-    }else{
-        console.log('Nobody won this game!');
-    }
-
-}
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
-    console.log(`You: ${humanChoice} vs Computer: ${computerChoice}`);
-
     if(humanChoice == computerChoice)
     {
-        console.log('Nobody won this round!');
         return "nobody";
     }
     else if(humanChoice == "rock" && computerChoice == "scissors" ||
             humanChoice == "scissors" && computerChoice == "paper" ||
             humanChoice == "paper" && computerChoice == "rock")
     {
-        console.log('You won this round!');
         return "human";
     }
-    else if(humanChoice == "rock" && computerChoice == "paper" ||
-            humanChoice == "scissors" && computerChoice == "rock" ||
-            humanChoice == "paper" && computerChoice == "scissors")
+    else
     {
-        console.log('You lost this round!');
         return "computer";
     }
 }
 
-function isValidChoice(choice){
-    if (choice == null) return false;
-    choice = choice.toLowerCase();
-    if(choice != "rock" && choice != "paper" && choice != "scissors"){
-        return false;
-    }
-    return true;
-}
-
-function getHumanChoice(){
-    let choice;
-    while(true){
-        choice = prompt("Choose 'rock', 'paper' or 'scissors':", '');
-        if(isValidChoice(choice)){
-            return choice;
-        }else{
-            alert("Uncorrect option!");
-            continue;
-        }
-    }
-}
-
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
+    let choices = ["rock", "paper", "scissors"];
+    let selectedChoice = Math.floor(Math.random() * 3);
+    
+    return choices[selectedChoice];
+}
 
-    switch(choice){
-        case 0: return "rock";
-        case 1: return "paper";
-        case 2: return "scissors";
+function toggleBtnsVisibility(){
+    choiceButtons.forEach((button) => {
+        button.classList.toggle("visibility");
+    });
+    resetBtn.classList.toggle("visibility");
+}
+
+function resetAll(){
+    humanScore = computerScore = 0;
+    humanScoreSpan.textContent = "0";
+    computerScoreSpan.textContent = "0";
+    logs.textContent = "";
+    winnerMessage.textContent = "";
+    toggleBtnsVisibility();
+}
+
+function updateScore(winner){
+    switch(winner){
+        case "nobody": 
+            winnerMessage.textContent = "Nobody won this round!";
+            break;
+        case "human": 
+            humanScore++;
+            humanScoreSpan.textContent = humanScore;
+            winnerMessage.textContent = "Human won this round!";
+            break;
+        case "computer": 
+            computerScore++;
+            computerScoreSpan.textContent = computerScore;
+            winnerMessage.textContent = "Computer won this round!";
+            break;
     }
 }
 
-playGame();
+function updateLogs(humanChoice, computerChoice){
+    const logsItem = document.createElement("li");
+    logsItem.textContent = `${humanChoice} vs ${computerChoice}`;
+    logs.appendChild(logsItem);
+}
+
+const choicesContainer = document.querySelector(".choices-container");
+const humanScoreSpan = document.querySelector("#human-score");
+const computerScoreSpan = document.querySelector("#computer-score");
+const logs = document.querySelector("#logs");
+const winnerMessage = document.querySelector("#winner-message");
+const choiceButtons = document.querySelectorAll(".choices-btns");
+const resetBtn = document.querySelector("#reset");
+
+choicesContainer.addEventListener("click",(event) =>{
+    let humanChoice = event.target.id;
+    if(humanChoice === "") return;
+    let computerChoice = getComputerChoice();
+    let winner;
+
+    switch(humanChoice){
+        case "rock": 
+            winner = playRound("rock", computerChoice);
+            break;
+        case "paper": 
+            winner = playRound("paper", computerChoice);
+            break;
+        case "scissors": 
+            winner = playRound("scissors", computerChoice);
+            break;
+        case "reset":
+            resetAll();
+            return;
+    }
+
+    updateScore(winner);
+
+    updateLogs(humanChoice, computerChoice);
+    
+    if(humanScore === 5){
+        winnerMessage.textContent = "Human won this game!";
+        toggleBtnsVisibility()
+    }else if(computerScore === 5){
+        winnerMessage.textContent = "Computer won this game!";
+        toggleBtnsVisibility()
+    }
+});
+
